361ff3388f26270ec8150ddd5f8e5c8617b3ed6b
Kima-code <114276291+Kima-code@users.noreply.github.com>
2022-12-03 09:51:53 +0700

    Added 2 Skripts: MainMenu&GameOver

---
+++ Assets/Skripts/GameOver.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LoadLevel : MonoBehaviour 
+{
+    // время до загрузки уровня
+    public float delay = 3f;
+
+    // имя загружаемого уровня
+    public string levelName;
+
+    public IEnumerator Start()
+    {
+        // задержка на заданное число секунд
+        yield return new WaitForSeconds(delay);
+
+        // загрузка уровня с указанным именем
+        SceneManager.LoadScene("MainMenu");
+    }
+}
